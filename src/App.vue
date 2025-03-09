11c84289a89f12cf1c05754866247820817c26f4
Sebastian <sebastianlakla@gmail.com>
2025-03-09 18:01:35 +0100

    Add component with font

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import chartComponent from './components/chart-component.vue';
+import fontsComponent from "./components/font-component.vue";
 export default {
     name: 'App',
     components: {
         chartComponent,
+        fontsComponent
     }
 }
 </script>
 
 <template>
+
 <chartComponent></chartComponent>
 </template>
 
