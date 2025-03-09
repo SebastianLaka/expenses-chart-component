5b032b65dafb55bc414bcd80a5a8fcb17db2c258
Sebastian <sebastianlakla@gmail.com>
2025-03-09 17:47:05 +0100

    Creating testing lines of code. To check do it all works.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script >
+<script>
+import chartComponent from './components/chart-component.vue';
+export default {
+    name: 'App',
+    components: {
+        chartComponent,
+    }
+}
 </script>
 
 <template>
+<chartComponent></chartComponent>
 </template>
 
 <style lang="scss">
-
 </style>
---
+++ src/components/chart-component.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus commodi temporibus doloribus molestias dignissimos illum, sed perspiciatis ex dolorem obcaecati?</p>
+</template>
+<style lang="scss" scoped>
+$color: red;
+p{
+    color: $color;
+}
+</style>
+<script>
+export default {
+    name: 'chartComponent',
+};
+</script>
