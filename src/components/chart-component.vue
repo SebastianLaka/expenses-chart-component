87bf770082280fcaf7394ce950106ca49aefbb20
Sebastian <sebastianlakla@gmail.com>
2025-03-10 19:32:48 +0100

    Add font sizes and weights in new file fonts

--- src/components/chart-component.vue
+++ src/components/chart-component.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="chart-component">
     <div class="chart-header">
-      <p class="chart-header__balance">My balance</p>
-      <p class="chart-header__amount-balance">$921.48</p>
+      <div class="chart-header-balance">
+        <p class="chart-header-balance__balance">My balance</p>
+        <p class="chart-header-balance__amount-balance">$921.48</p>
+      </div>
     </div>
     <div class="chart-body">
       <p class="chart-body-header">Spending - Last 7 days</p>
@@ -15,6 +17,7 @@
 </template>
 <style lang="scss" scoped>
 @import '@/assets/colors.scss';
+@import '@/assets/fonts.scss';
 .chart-component {
   display: flex;
   justify-content: center;
@@ -22,12 +25,18 @@
   flex-direction: column;
   height: 100svh;
   .chart-header {
-    background-color: $soft-red;
-    &__balance,
-    &__amount-balance {
-      color: $cream;
+    .chart-header-balance {
+      background-color: $soft-red;
+      &__balance,
+      &__amount-balance {
+        color: $cream;
+        font-size: $primary-size;
+      }
+      &__amount-balance{
+        font-size: $secondary-size;
+        font-weight: $font-bold-size;
+      }
     }
-
   }
 }
 </style>
