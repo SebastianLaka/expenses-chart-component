1dbe08fa38785e93a250f9d75ffa2b999c3c9214
Sebastian <sebastianlakla@gmail.com>
2025-03-10 19:16:25 +0100

    Add structure of header and body in chart component with primary styles to center and colors

--- src/components/chart-component.vue
+++ src/components/chart-component.vue
@@ -1,15 +1,38 @@
 <template>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus commodi temporibus doloribus molestias dignissimos illum, sed perspiciatis ex dolorem obcaecati?</p>
+  <div class="chart-component">
+    <div class="chart-header">
+      <p class="chart-header__balance">My balance</p>
+      <p class="chart-header__amount-balance">$921.48</p>
+    </div>
+    <div class="chart-body">
+      <p class="chart-body-header">Spending - Last 7 days</p>
+    </div>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus commodi temporibus doloribus
+    molestias dignissimos illum, sed perspiciatis ex dolorem obcaecati?
+  </p>
 </template>
 <style lang="scss" scoped>
 @import '@/assets/colors.scss';
-p{
-    color: $cyan;
+.chart-component {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100svh;
+  .chart-header {
+    background-color: $soft-red;
+    &__balance,
+    &__amount-balance {
+      color: $cream;
+    }
+
+  }
 }
 </style>
 <script>
-
 export default {
-    name: 'chartComponent',
-};
+  name: 'chartComponent',
+}
 </script>
