b7ddf80a4a2db132d16a01ecec4a7e59b25c36ff
Sebastian <sebastianlakla@gmail.com>
2025-04-02 18:23:04 +0200

    Add new media with 475px with styles

--- src/components/chart-component.vue
+++ src/components/chart-component.vue
@@ -7,19 +7,17 @@
           <p class="chart-header-balance__amount-balance">$921.48</p>
         </div>
         <div class="chart-header-icon-container">
-          <img src="../assets/icons/logo.svg" alt="Component logo" class="logo"/>
+          <img src="../assets/icons/logo.svg" alt="Component logo" class="logo" />
         </div>
       </div>
       <div class="body-component">
         <p class="body-component__header">Spending - Last 7 days</p>
         <div class="chart-diagrams">
-          <div class="mon-chart"  v-for="(chartItem, index) in data" 
-          :key="index">
-          <p class="mon-chart__day">{{ chartItem.day }}</p>
-          <div class="mon-chart__chart" :style="{ height: chartItem.amount * 2.5  + 'px' }"></div>
-          <p class="mon-chart__chart-value">%{{ chartItem.amount }}</p>
+          <div class="mon-chart" v-for="(chartItem, index) in data" :key="index">
+            <p class="mon-chart__day">{{ chartItem.day }}</p>
+            <div class="mon-chart__chart" :style="{ height: chartItem.amount * 2.5 + 'px' }"></div>
+            <p class="mon-chart__chart-value">%{{ chartItem.amount }}</p>
           </div>
-          
         </div>
         <div class="chart-body-footer">
           <div class="chart-total">
@@ -39,100 +37,139 @@
 @import '@/assets/colors.scss';
 @import '@/assets/fonts.scss';
 @media (min-width: 375px) {
-  body{
-  .main-component {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100svh;
-    padding: .4em;
-    .chart-component {
+    .main-component {
       display: flex;
-      justify-content: space-between;
-      flex-direction: column;
-      flex-wrap: wrap;
-      min-width: 100%;
-      .chart-header {
+      align-items: center;
+      justify-content: center;
+      height: 100svh;
+      padding: 0.4em;
+      .chart-component {
         display: flex;
         justify-content: space-between;
-        background-color: $soft-red;
-        padding: 1em;
-        border-radius: 0.8em;
-        .chart-header-balance {
-          &__balance,
-          &__amount-balance {
-            color: $cream;
-            font-size: $mobile-font-size;
-            font-weight: $font-weight-size;
-          }
-          &__amount-balance {
-            font-size: $primary-size;
-            font-weight: $font-bold-size;
-          }
-        }
-        .chart-header-icon-container {
-          display: flex;
-          align-items: center;
-        }
-      }
-      .body-component {
-        display: flex;
         flex-direction: column;
-        background-color: $very-pale-orange;
-        border-radius: 0.8em;
-        gap: 2em;
-        margin-top: .5em;
-        &__header {
-          text-align: center;
-          font-weight: $font-bold-size;
-          font-size: $primary-size;
-        }
-        .chart-diagrams {
+        flex-wrap: wrap;
+        min-width: 100%;
+        .chart-header {
           display: flex;
-          flex-direction: row;
           justify-content: space-between;
-          .mon-chart{
-            display: flex;
-            flex-direction: column-reverse;
-            align-items: center;
-            &__day {
-              color: $medium-brown;
-            }
-            &__chart {
-              background-color: $soft-red;
-              width: 2em;
-              border-radius: 0.4em;
-              
-            }
-            &__chart-value{
-              color: $very-pale-orange;
-              background-color: $dark-brown;
-              padding: .3em;
-              margin-bottom: .5em;
-              border-radius: .4em;
-              font-family: inherit;
+          background-color: $soft-red;
+          padding: 1em;
+          border-radius: 0.8em;
+          .chart-header-balance {
+            &__balance,
+            &__amount-balance {
+              color: $cream;
               font-size: $mobile-font-size;
+              font-weight: $font-weight-size;
+            }
+            &__amount-balance {
+              font-size: $primary-size;
+              font-weight: $font-bold-size;
             }
           }
+          .chart-header-icon-container {
+            display: flex;
+            align-items: center;
+          }
         }
-        .chart-body-footer {
+        .body-component {
           display: flex;
-          justify-content: space-between;
-          .chart-total,
-          .chart-increase {
-            &__amount {
-              font-size: $primary-size;
+          flex-direction: column;
+          background-color: $very-pale-orange;
+          border-radius: 0.8em;
+          gap: 2em;
+          margin-top: 0.5em;
+          &__header {
+            text-align: center;
+            font-weight: $font-bold-size;
+            font-size: $primary-size;
+          }
+          .chart-diagrams {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            .mon-chart {
+              display: flex;
+              flex-direction: column-reverse;
+              align-items: center;
+              &__day {
+                color: $medium-brown;
+              }
+              &__chart {
+                background-color: $soft-red;
+                width: 2em;
+                border-radius: 0.4em;
+              }
+              &__chart-value {
+                color: $very-pale-orange;
+                background-color: $dark-brown;
+                padding: 0.3em;
+                margin-bottom: 0.5em;
+                border-radius: 0.4em;
+                font-family: inherit;
+                font-size: $mobile-font-size;
+              }
             }
-            &__title {
-              color: $medium-brown;
-              font-size: $mobile-font-size;
+          }
+          .chart-body-footer {
+            display: flex;
+            justify-content: space-between;
+            .chart-total,
+            .chart-increase {
+              &__amount {
+                font-size: $primary-size;
+              }
+              &__title {
+                color: $medium-brown;
+                font-size: $mobile-font-size;
+              }
             }
           }
         }
       }
     }
   }
-}
+
+@media (min-width: 475px) {
+  .main-component {
+    .chart-component {
+      .chart-header{
+        .chart-header-balance{
+          &__balance{
+            font-size: $primary-size;
+          }
+          &__amount-balance{
+            font-size: $secondary-size;
+          }
+        }
+      }
+      .body-component {
+        padding: 1.5em;
+        &__header{
+          font-size: $secondary-size;
+        }
+        .chart-diagrams {
+          .mon-chart {
+            &__chart {
+              min-width: 2.5em;
+            }
+          }
+        }
+        .chart-body-footer {
+            .chart-total,
+            .chart-increase {
+              &__amount {
+                font-size: $secondary-size;
+              }
+              &__title {
+                color: $medium-brown;
+                font-size: $primary-size;
+              }
+            }
+          }
+      }
+    }
+  }
 }
 </style>
 <script>
